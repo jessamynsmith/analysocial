91cbd0e4990417e508b5d63dcd5be2993cb20b50
Jessamyn Smith <geekchick77@gmail.com>
2016-07-26 22:42:56 -0400

    Make icon go to home screen, and make usage the home screen.

--- graph/urls.py
+++ graph/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import RedirectView
 from graph import views as graph_views
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='accounts/profile/')),
+    url(r'^$', RedirectView.as_view(url='usage/')),
     url(r'^privacy/', graph_views.PrivacyView.as_view(), name="privacy"),
     url(r'^accounts/profile/', graph_views.UserProfileView.as_view(), name="account_profile"),
     url(r'^usage/', graph_views.UsageView.as_view(), name="usage"),

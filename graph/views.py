d7e1c636f5949e30b08be1e3bba4c4c956e5e304
Jessamyn Smith <geekchick77@gmail.com>
2016-07-26 23:30:55 -0400

    Added info on getting your account deleted.

--- graph/views.py
+++ graph/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
@@ -7,6 +8,11 @@ import facebook
 
 class PrivacyView(TemplateView):
     template_name = 'account/privacy.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PrivacyView, self).get_context_data(**kwargs)
+        context['admin_email'] = settings.ADMINS[0][1]
+        return context
 
 
 @method_decorator(login_required, name='dispatch')

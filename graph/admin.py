3c2dfa491f9c31097d69605ea7a2190ad477a3ad
Jessamyn Smith <geekchick77@gmail.com>
2016-09-08 12:04:55 -0400

    Added date range filter to admin.

--- graph/admin.py
+++ graph/admin.py
@@ -1,11 +1,12 @@
+from daterange_filter.filter import DateRangeFilter
 from django.contrib import admin
 
 from graph import models as graph_models
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['user', 'id', 'created_time', 'message']
-    list_filter = ['user']
+    list_display = ['id', 'user', 'created_time', 'message']
+    list_filter = ['user', ('created_time', DateRangeFilter)]
 
 
 admin.site.register(graph_models.Post, PostAdmin)

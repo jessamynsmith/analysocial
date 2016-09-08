9a0fc7985d1634b08a77fbea62b22c8ad26bfdd1
Jessamyn Smith <geekchick77@gmail.com>
2016-09-07 23:41:07 -0400

    Moved account signal to users app.

--- graph/models.py
+++ graph/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.template.defaultfilters import truncatewords
 from django.conf import settings
 
-from allauth.socialaccount import signals as allauth_signals
-
 
 class Post(models.Model):
     id = models.CharField(max_length=40, primary_key=True)
@@ -15,12 +13,3 @@ class Post(models.Model):
     def __str__(self):
         message = truncatewords(self.message, 10)
         return '%s: %s (%s - %s)' % (self.user, message, self.created_time, self.id)
-
-
-def update_user_email(request, sociallogin, **kwargs):
-    if not sociallogin.user.email:
-        sociallogin.user.email = sociallogin.email_addresses[0].email
-        sociallogin.user.save()
-
-
-allauth_signals.pre_social_login.connect(update_user_email)

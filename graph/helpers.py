8c1bb69e3b482f38c471bdf42cabbe15aa6559f9
Jessamyn Smith <geekchick77@gmail.com>
2016-09-09 16:35:06 -0400

    Updated helpers to deal with posts, attachments, comments, and comments on comments.

--- graph/helpers.py
+++ graph/helpers.py
@@ -21,70 +21,54 @@ def retrieve_facebook_posts(user=None, retrieve_all=False, ignore_errors=False):
         access_token = social_account.socialtoken_set.all()[0].token
         version = settings.FACEBOOK_API_VERSION
         graph_api = facebook.GraphAPI(access_token=access_token, version=version)
-        posts = graph_api.request('me/posts/')
+        request_path = 'me/posts/'
+        posts = graph_api.request(request_path)
 
         while 'data' in posts:
             for post_data in posts['data']:
                 post_data['user'] = social_account.user
-                post = graph_models.Post(**post_data)
                 try:
+                    post = graph_models.Post(**post_data)
                     post.save()
+
+                    try:
+                        attachments = graph_api.request('%s/attachments/' % post_data['id'])
+                        for attachment_data in attachments['data']:
+                            attachment_data['post'] = post
+                            attachment = graph_models.Attachment(**attachment_data)
+                            attachment.save()
+                    except Exception as e:
+                        if not ignore_errors:
+                            raise e
+
+                    try:
+                        comments = graph_api.request('%s/comments/' % post_data['id'])
+                        for comment_data in comments['data']:
+                            comment_data['post'] = post
+                            comment_data['from_json'] = comment_data.pop('from')
+                            comment = graph_models.Comment(**comment_data)
+                            comment.save()
+                            replies = graph_api.request('%s/comments/' % comment_data['id'])
+                            for reply_data in replies['data']:
+                                reply_data['post'] = post
+                                reply_data['comment'] = comment
+                                reply_data['from_json'] = reply_data.pop('from')
+                                reply = graph_models.Comment(**reply_data)
+                                reply.save()
+                    except Exception as e:
+                        if not ignore_errors:
+                            raise e
+
                 except IntegrityError as e:
                     if str(e).find('duplicate key value') >= 0:
                         if not ignore_errors:
                             posts = {}
                             break
-                    else:
-                        print(request_path)
-                        raise e
+                    raise e
                 except Exception as e:
                     print(request_path)
                     print(post_data)
                     raise e
-
-                try:
-                    attachments = graph_api.request('%s/attachments/' % post_data['id'])
-                except:
-                    print(post_data)
-                    exc_type, exc_value, exc_traceback = sys.exc_info()
-                    traceback.print_tb(exc_traceback, file=sys.stdout)
-                for attachment_data in attachments['data']:
-                    attachment_data['post'] = post
-                    try:
-                        attachment = graph_models.Attachment(**attachment_data)
-                        attachment.save()
-                    except IntegrityError as e:
-                        if not str(e).find('duplicate key value') >= 0:
-                            print(request_path)
-                            print(attachment_data)
-                            raise e
-                    except Exception as e:
-                        print(request_path)
-                        print(attachment_data)
-                        raise e
-
-                try:
-                    comments = graph_api.request('%s/comments/' % post_data['id'])
-                except:
-                    print(request_path)
-                    print(post_data)
-                    exc_type, exc_value, exc_traceback = sys.exc_info()
-                    traceback.print_tb(exc_traceback, file=sys.stdout)
-                for comment_data in comments['data']:
-                    comment_data['post'] = post
-                    try:
-                        comment_data['from_json'] = comment_data.pop('from')
-                        comment = graph_models.Comment(**comment_data)
-                        comment.save()
-                    except IntegrityError as e:
-                        if not str(e).find('duplicate key value') >= 0:
-                            print(request_path)
-                            print(comment_data)
-                            raise e
-                    except Exception as e:
-                        print(request_path)
-                        print(comment_data)
-                        raise e
 
             if not retrieve_all or not posts:
                 break
